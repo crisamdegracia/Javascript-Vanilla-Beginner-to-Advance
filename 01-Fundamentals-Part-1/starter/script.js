0b2724f0c5552fc72cc961a253148858c0d01e2f
Crisam De Gracia <crisamdegracia@gmail.com>
2020-12-31 02:56:49 +0800

    working in functions

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -5,10 +5,8 @@
 // 	johnMass = 69,
 // 	johnHeight = 2.7,
 
-
 // 	markBMI = markMass / (markHeight ** 2);
 // 	johnBMI = johnMass / (markHeight ** 2);
-
 
 // 	isHighier = markBMI > johnBMI;
 
@@ -16,10 +14,6 @@
 // 	console.log("Johns's BMI is : " + johnBMI )
 // 	console.log("---------------------------------------------");
 // 	console.log( "Is Mark's BMI higher than John's? " + isHighier )
-
-
-
-
 
 /* 
 
@@ -71,25 +65,109 @@ const day = 'friday';
 
  */
 
- // CHALLENGE #2
+// CHALLENGE #2
 
- var john = (89 + 120 + 103) / 3;
- var mike = (116 + 94 + 123) / 3;
+//  var john = (89 + 120 + 103) / 3;
+//  var mike = (116 + 94 + 123) / 3;
 
-//   john > mike  ?  console.log( 'Team John has an average score: ' + john + 'and a winner!'  ) : console.log('Team Mike has an average score: ' + mike + ' and a winner!');
-//   john === mike ? console.log(' Too close! ') : console.log('It\'s a draw! ');
+// //   john > mike  ?  console.log( 'Team John has an average score: ' + john + 'and a winner!'  ) : console.log('Team Mike has an average score: ' + mike + ' and a winner!');
+// //   john === mike ? console.log(' Too close! ') : console.log('It\'s a draw! ');
 
-	console.log ('SCORE: John - ' + john + '  SCORE : Mike ' + mike);
-  if( john > mike ){
-	console.log( 'Team John has an average score: ' + john + ' and a winner!'  )
-  }
-  else if ( john < mike ){
-	console.log( 'Team Mike has an average score: ' + mike + ' and a winner!'  )
-  }
-  else if (john === mike ){
-	  console.log(' It\'s a draw!');
-  }
-  else {
-	  console.log( 'nobody knows why whe\'re here');
-  }
-  
+// 	console.log ('SCORE: John - ' + john + '  SCORE : Mike ' + mike);
+//   if( john > mike ){
+// 	console.log( 'Team John has an average score: ' + john + ' and a winner!'  )
+//   }
+//   else if ( john < mike ){
+// 	console.log( 'Team Mike has an average score: ' + mike + ' and a winner!'  )
+//   }
+//   else if (john === mike ){
+// 	  console.log(' It\'s a draw!');
+//   }
+//   else {
+// 	  console.log( 'nobody knows why whe\'re here');
+//   }
+
+// var WhatIDo = function(job, firstName){
+// 	switch(job){
+// 		case 'teacher':
+// 			return firstName + ' teaches kids to code';
+// 		case 'driver':
+// 			return firstName + ' drives a cab in Lisbon'
+// 		case 'designer':
+// 			return firstName + ' designs beautiful websites';
+// 		default:
+// 		return firstName + ' does something else';
+
+// 	}
+// }
+
+// console.log(WhatIDo('teacher', 'John'))
+
+// Challenge
+
+// function calculator(_bill) {
+// 	var _tip = 0;
+// 	var _allTips = [];
+// 	var _totalAmnt = [];
+
+// 	if (_bill <= 50) {
+// 		_tip = _bill * 0.2;
+// 		_allTips.push(_tip);
+// 		_totalAmnt.push(_tip + _bill);
+// 		console.log('Your tip is : $' + _tip);
+
+// 	} else if (_bill >= 51 && _bill <= 200) {
+// 		_tip = _bill * 0.15;
+// 		_allTips.push(_tip);
+// 		_totalAmnt.push(_tip + _bill);
+// 		console.log('Your tip is : $' + _tip + ' for the bill ' + _bill);
+
+
+// 	} else if( _bill > 200) {
+// 		_tip = _bill * 0.1;
+// 		_allTips.push(_tip);
+// 		_totalAmnt.push(_tip + _bill);
+// 		console.log('Your tip is : $' + _tip);
+// 	}
+
+// 	console.log('Total bill : $' + _bill);
+// 	console.log( 'All the tips : $' + _allTips);
+// 	console.log( 'All the amount : $' + _totalAmnt);
+// 	console.log('-----------------------------------');
+
+// }
+
+// calculator(43);
+// calculator(67);
+// calculator(203);
+
+
+//Solution -------
+
+function tipCalculator(_bill){
+var percentage;
+
+if(_bill < 50 ){
+	pencentage = .2;
+
+}
+else if (_bill >= 50 && _bill < 200 ){
+	percentage = .15;
+} else {
+	percentage = .1;
+}
+return percentage * _bill;
+}
+
+
+var bills = [124, 50, 268];
+var tips = [tipCalculator(bills[0]),
+			tipCalculator(bills[1]),
+			tipCalculator(bills[2])];
+
+			var finalValues = [	bills[0]+ tips[0],
+								bills[1]+ tips[1],
+								bills[2]+ tips[2] ];
+
+
+			console.log(tips, finalValues);
