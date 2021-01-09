5a619d361452f44697ddd5c58aaa5ddf89ab9a84
Crisam De Gracia <crisamdegracia@gmail.com>
2021-01-09 20:05:21 +0800

    First class function, IIFE, Closure

--- complete-javascript-course-master/14-OOP/starter/script.js
+++ complete-javascript-course-master/14-OOP/starter/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// Function constructor 
+// Function constructor
 
 /*
 //Constructor
@@ -24,7 +24,6 @@ Person.prototype.calculateAge = function (){
 
 mark.calculateAge()
 */
-
 
 //Object.create
 
@@ -56,10 +55,8 @@ The difference between object.create and function constructor
 	from the constructor's prototype
 */
 
-
-
 // Primitives vs Objects
-
+/* 
 The big difference between Primitive and Objects
 	is that variables containing primitives actually hold
 	that data inside of variable itself 
@@ -73,4 +70,91 @@ On objects it very different
 - AGAIN -
 	a variable declared as an object
 	does not have a real copy of the object 
-	it points to that object! AHA!
+	it points to that object! AHA! */
+
+/* 
+	Functions returning function
+	
+
+	function interviewJobs(job){
+		if(job === 'designer'){
+			return function(name){
+				console.log(name + ', can you please explain that UX design is?');
+			}
+		}
+		else if( job === 'teacher'){
+			return function(name){
+				console.log('What subject do you teach ' + name + '?');
+			}
+		} else {
+			return function(name){
+				console.log('Hello ' + name + ', what do you do?');
+			}
+		}
+	}
+
+	// store them in variable so we can us the name on returning function
+
+	var teacherQuestion = interviewJobs('teacher');
+	var designerQuestion = interviewJobs('designer');
+
+	teacherQuestion('John');
+	designerQuestion('Mark');
+
+	interviewJobs('teacher')('Grace');
+
+	*/
+
+/* 
+	CLOSURE - an inner function has always access to the variable
+	and parameters of its outer function, even after the outer function
+	has returned	
+
+		- even after return or remove from the execution context
+		the outer function can still have an access to the function
+		and that is called CLOSURE
+	*/
+
+function retirement(retirementAge) {
+  var a = ' years left until retirement.';
+
+  return function (yearOfBirth) {
+    var age = 2021 - yearOfBirth;
+    console.log(retirementAge - age + a);
+  };
+}
+
+//two ways to call
+
+//1
+var retirementUS = retirement(66);
+retirementUS(1990);
+var retirementGermany = retirement(65);
+retirementGermany(2020);
+var retirementIceland = retirement(67);
+retirementIceland(1990);
+//2
+retirement(66)(1990);
+
+
+
+function interviewJobs(jobs){
+	var designer = ' Can you please explain what UX is?';
+	var teacher  = ' What subject do you teach? ';
+	var noOne 	= ' Hello, what do you do? ';
+
+	return function(name){
+		if(jobs == 'designer'){
+			console.log(name + designer);
+		}else if(jobs == 'teacher'){
+			console.log(teacher + name);
+		} else {
+			console.log(noOne + name)
+		}
+	}
+}
+
+var interviewDesigner = interviewJobs('designer');
+interviewDesigner('John');
+var interviewTeacher = interviewJobs('teacher');
+interviewTeacher('Merry');
